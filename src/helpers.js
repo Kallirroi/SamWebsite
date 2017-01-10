3f4a3eec3bc88599670c80a1b461f5c4195f8e4d
Kallirroi <kallirroi.retzepi@gmail.com>
2017-01-10 00:26:16 -0500

    cleanup

--- src/helpers.js
+++
@@ -1,8 +0,0 @@
-export const findContentForMode = (cMap, percent) => {
-  for (let i = cMap.length - 1; i >= 0; i--) {
-    if (percent >= cMap[i].fromPosition) {
-      return cMap[i].text;
-    }
-  }
-  return cMap[0].text;
-};
