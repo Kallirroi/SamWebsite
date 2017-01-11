e56c74fa39ebc31cb3e82534ed4a9410b172194a
Kallirroi <kallirroi.retzepi@gmail.com>
2017-01-10 20:35:55 -0500

    CSS fixes

--- src/Project.js
+++ src/Project.js
@@ -4,7 +4,6 @@ import './App.css';
 
 class Project extends React.Component {
 
-
 	componentDidMount(){
 		this.ref.addEventListener('click', () => {
      	 this.setState({current: this.props.id});
@@ -17,7 +16,6 @@ class Project extends React.Component {
 	        	<div className="Doc" dangerouslySetInnerHTML={ {__html: this.props.doc} }></div> 
 	        	<img src={this.props.imageURL} role="presentation"/> 
 	        	<div className="ProjectItemDetails" >
-	        		<div  className="ProjectItemDetails-Name"> {this.props.name}</div>
 	        		<div className="ProjectItemDetails-Caption">
 	        			{this.props.caption}
 	        		</div>
