411e83a8654c8bcb0c4e6e782df85947c59c6861
Kallirroi <kallirroi.retzepi@gmail.com>
2017-01-14 13:21:50 -0500

    added draggable behaviour

--- src/Item.js
+++ src/Item.js
@@ -1,29 +1,62 @@
 import React from 'react';
 import './App.css';
 
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 
 class Item extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			isMouseInside: false
+			isMouseInside: false,
+			activeDrags: 0,
+	        deltaPosition: {
+	          x: 0, y: 0
+	        }
 		};
 		this.showDetails=this.showDetails.bind(this);
 		this.mouseEnter = this.mouseEnter.bind(this);
 		this.mouseExit = this.mouseExit.bind(this);
+		this.handleDrag = this.handleDrag.bind(this);
+		this.onStart = this.onStart.bind(this);
+		this.onStop = this.onStop.bind(this);
 	}
+
+	handleDrag(e, ui) {
+      const {x, y} = this.state.deltaPosition;
+      this.setState({
+        deltaPosition: {
+          x: x + ui.deltaX,
+          y: y + ui.deltaY,
+        }
+      });
+    }
+
+    onStart() {
+      this.setState({activeDrags: ++this.state.activeDrags});
+    }
+
+    onStop() {
+      this.setState({activeDrags: --this.state.activeDrags});
+    }
 
 	componentDidMount(){
 		this.ref.addEventListener('click', this.showDetails, true);
 		this.ref.addEventListener('mouseEnter', this.mouseEnter, true);
 		this.ref.addEventListener('mouseExit', this.mouseExit, true);
+		this.ref.addEventListener('handleDrag', this.handleDrag, true);
+		this.ref.addEventListener('onStart', this.onStart, true);
+		this.ref.addEventListener('onStop', this.onStop, true);
 	}
 
 	componentWillUnmount(){
 		this.ref.removeEventListener('click', this.showDetails, true);
 		this.ref.removeEventListener('mouseEnter', this.mouseEnter, true);
 		this.ref.removeEventListener('mouseExit', this.mouseExit, true);
+		this.ref.removeEventListener('handleDrag', this.handleDrag, true);
+		this.ref.removeEventListener('onStart', this.onStart, true);
+		this.ref.removeEventListener('onStop', this.onStop, true);
 	}	
 
 	showDetails(e) {
@@ -39,23 +72,34 @@ class Item extends React.Component {
 	}
 
 	render() {
+		const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
+		const {deltaPosition, controlledPosition} = this.state;
+
 		let classNameItemType = this.state.isMouseInside ? "ItemType ItemTypeIsHidden" : "ItemType"; {/*FLIPPING the classNameItemType with the classNameItelDetails*/}
 		let classNameItemDetails = this.state.isMouseInside ? "ItemDetails" : "ItemDetails ItemDetailsIsHidden";
 		return (
-	        <div className={this.props.type} onMouseEnter={this.mouseEnter} onMouseOut={this.mouseExit} ref={ref => { this.ref = ref; }} onClick={this.showDetails} > 
-	        	<div className="player" dangerouslySetInnerHTML={ {__html: this.props.soundcloud} }></div>
-	        	<img src={this.props.imageURL} role="presentation" className={this.props.imagesAnxiety}/>
-	        	<div className={classNameItemType}>  
-	        		<p>{this.props.name} </p>
-	        		<p>{this.props.type} </p>
-	        	</div>
-	        	<div className={classNameItemDetails} onMouseEnter={this.mouseEnter} onMouseOut={this.mouseExit}  > 
-	        		{this.props.details1}<br/><br/>
-        			{this.props.details2}<br/><br/>
-        			{this.props.details3}<br/><br/>
-        			{this.props.details4}        			
-	        	</div> 
-	        </div>
+		<Draggable
+            zIndex={100}
+            onStart={this.handleStart}
+            onDrag={this.handleDrag}
+            onStop={this.handleStop}>
+            <div className="box no-cursor">
+		        <div className={this.props.type} onMouseEnter={this.mouseEnter} onMouseOut={this.mouseExit} ref={ref => { this.ref = ref; }} onClick={this.showDetails} > 
+		        	<div className="player" dangerouslySetInnerHTML={ {__html: this.props.soundcloud} }></div>
+		        	<img src={this.props.imageURL} role="presentation" className={this.props.imagesAnxiety}/>
+		        	<div className={classNameItemType}>  
+		        		<p>{this.props.name} </p>
+		        		<p>{this.props.type} </p>
+		        	</div>
+		        	<div className={classNameItemDetails} onMouseEnter={this.mouseEnter} onMouseOut={this.mouseExit}  > 
+		        		{this.props.details1}<br/><br/>
+	        			{this.props.details2}<br/><br/>
+	        			{this.props.details3}<br/><br/>
+	        			{this.props.details4}        			
+		        	</div> 
+		        </div>
+            </div>
+        </Draggable>
 	    )
 	}
 }
--- src/Title.js
+++ src/Title.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 
-
 class Title extends React.Component {
 
 	render() {
 		return (
-	        <div className={["ImagesAnxiety", "Title"].join(' ')} > 
-	        	archive
-	        </div>
+		        <div className={["ImagesAnxiety", "Title"].join(' ')} > 
+		        	archive
+		        </div>
 	    )
 	}
 }
